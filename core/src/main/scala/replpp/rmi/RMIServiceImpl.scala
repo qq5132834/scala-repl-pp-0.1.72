d8e56d8a6f523375433d4ab6c8763b106ff07e05
qq5132834 <513283439@qq.com>
2023-10-31 17:06:17 +0800

    植入RMI

--- core/src/main/scala/replpp/rmi/RMIServiceImpl.scala
+++ core/src/main/scala/replpp/rmi/RMIServiceImpl.scala
@@ -10,22 +10,28 @@ class RMIServiceImpl(config: Config) extends UnicastRemoteObject
   with replpp.rmi.RMIService {
 
   override def callBack1(line: String): String = {
-    this.run(this.config, line)
-    "hello, " + line
+    var res = this.run(this.config, line)
+//    "hello, " + line
+    res
   }
 
 
-  def run(config: Config, line: String): Unit = {
+  def run(config: Config, line: String): String = {
 
     val predefCode = allPredefCode(config) //预定义代码
     val compilerArgs = replpp.compilerArgs(config)
+    val rmiOutputStream = new RmiOutputStream("D:/test.txt", "")
+//    rmiOutputStream.setStr("")
     import config.colors
     val rmiDriver = new RMIDriver(
       compilerArgs,
+//      scala.Console.out,
+      rmiOutputStream,
       onExitCode = config.onExitCode,
       greeting = config.greeting,
       prompt = config.prompt.getOrElse("scala"),
-      maxHeight = config.maxHeight
+      maxHeight = config.maxHeight,
+      None
     )
 
     val initialState: State = rmiDriver.initialState
@@ -36,12 +42,15 @@ class RMIServiceImpl(config: Config) extends UnicastRemoteObject
       throw new AssertionError(s"compilation error for predef code - error should have been reported above ^") with NoStackTrace
     }
 
-    //    rmiDriver.run("val a = 1 \n val b = 2 \n println(a+b) \n println(\"hello,run\")")(using state)
+//        rmiDriver.run("val a = 1 \n val b = 2 \n println(a+b) \n println(\"hello,run\")")(using state)
 //    rmiDriver.runQuietly("val a = 1 \n val b = 2 \n println(a+b) \n println(\"hello,runQuietly\") ")(using state)
 
-    System.err.println("hello, RMI.start")
-    rmiDriver.runQuietly(line)(using state)
+    System.err.println("hello, RMI.start:" + line)
+//    rmiDriver.runQuietly(line)(using state)
+    rmiDriver.run(line)(using state)
     System.err.println("hello, RMI.end")
+
+    rmiOutputStream.getStr
   }
 
 }
